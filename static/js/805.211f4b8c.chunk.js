7c13dd488a8427af27b851b380849d3398970ed8
Alyona-Nekhryshchyniuk <108234116+Alyona-Nekhryshchyniuk@users.noreply.github.com>
2023-02-21 10:44:34 +0000

    Deploying to gh-pages from @ Alyona-Nekhryshchyniuk/goit-react-hw-05-movies@f56c5ed1b0fe15d09ac6ecb40c501c774b74b940 🚀

---
+++ static/js/805.211f4b8c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[805],{805:function(e,r,t){t.r(r);var c=t(184);r.default=function(){return(0,c.jsx)("p",{children:"Sorry, no such page"})}}}]);
+//# sourceMappingURL=805.211f4b8c.chunk.js.map
